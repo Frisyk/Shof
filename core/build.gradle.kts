295b59d7bc4501345af1eee3f5d89ef479520b3b
frisyk <frisnadi1@gmail.com>
2024-02-08 11:34:17 +0700

    Initial commit: Shof App

---
+++ core/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("kotlin-parcelize")
+}
+
+apply("../shared_dependencies.gradle")
+
+android {
+    namespace = "com.dicoding.shof.core"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+        buildConfigField("String", "KEY", "\"b650046bf640e7bf7054093854b8d02a\"")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+//    implementation(project(":navigation"))
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //    Coroutines
+    val coroutinesVersion = "1.6.4"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    // Room
+    implementation("androidx.room:room-runtime:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+    ksp("androidx.room:room-compiler:2.5.2")
+
+
+
+    api("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+}
---
+++ core/src/main/java/com/dicoding/shof/core/di/CoreModule.kt
@@ -0,0 +1,57 @@
+package com.dicoding.shof.core.di
+
+import androidx.room.Room
+import com.dicoding.shof.core.data.local.LocalDataSource
+import com.dicoding.shof.core.data.local.room.GamesDatabase
+import com.dicoding.shof.core.data.remote.RemoteDataSource
+import com.dicoding.shof.core.data.remote.network.ApiService
+import com.dicoding.shof.core.domain.repository.IGamesRepository
+import com.dicoding.shof.core.utils.AppExecutors
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<GamesDatabase>().gamesDao() }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            GamesDatabase::class.java, "Games.db"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.rawg.io/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<IGamesRepository> {
+        com.dicoding.shof.core.data.GamesRepository(
+            get(),
+            get(),
+        )
+    }
+}
+
